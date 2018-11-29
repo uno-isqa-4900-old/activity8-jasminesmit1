3f39c9b3b88a7002f5c7fb0297e3600909159825
Jasmine Smith <jasminesmit1@unomaha.edu>
2018-11-28 21:56:20 -0600

    initial commit

--- activity8.py
+++
@@ -1,2 +0,0 @@
-# Activity 8
-# Erase the current code in the file and create the code to complete the requirements as stated in the assignment.
--- images/readme.md
+++
@@ -1,3 +0,0 @@
-# Images for this activity
-
-Place all images for this activity in this folder
